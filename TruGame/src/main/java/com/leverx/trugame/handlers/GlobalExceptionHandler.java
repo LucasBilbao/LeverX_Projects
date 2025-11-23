5ca603fdd2116df3100907af81f50892f7862344
Luka Bilbao <lucasbm789@gmail.com>
2025-11-23 21:04:21 +0400

    feat: add global exception handler
    Signed-off-by: Luka Bilbao <lucasbm789@gmail.com>

---
+++ TruGame/src/main/java/com/leverx/trugame/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.leverx.trugame.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String, Object>> handleValidationException(
+            MethodArgumentNotValidException ex
+    ) {
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        return ResponseEntity.badRequest().body(
+                Map.of(
+                        "message", "Validation Failed",
+                        "error", errors,
+                        "status", HttpStatus.BAD_REQUEST,
+                        "timestamp", Instant.now()
+                )
+
+        );
+    }
+}
